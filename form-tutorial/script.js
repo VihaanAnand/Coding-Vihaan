0964e6364b13f9c4c1648fa2107a6e13da5bad48
Untitled <vihaan.s.anand@gmail.com>
2024-06-08 12:47:54 -0700

    Create script.js

---
+++ form-tutorial/script.js
@@ -0,0 +1,18 @@
+const form = document.getElementById("form");
+form.addEventListener("submit", async function(event) {
+        event.preventDefault();
+        const formData = new FormData(form);
+        const data = Object.fromEntries(formData);
+        try {
+                const response = await fetch("https://script.google.com/macros/s/AKfycbwL7JhrulElTKjN9NLQj5N6YQECv_Uij6RDipDOQOsATMitGY4CAD9vcXksgJwcJ9lKMQ/exec3", {
+                        method: "POST",
+                        body: new URLSearchParams(data)
+                });
+                const responseText = await response.text();
+                alert(responseText);
+                form.reset();
+        }
+        catch (error) {
+                alert(error);
+        }
+});
