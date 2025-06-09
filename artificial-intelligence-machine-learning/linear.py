2e12fc2b3c9b908c978ce66bda5b078156c3aec0
Vihaan <vihaan.s.anand@gmail.com>
2025-06-09 07:44:15 -0500

    Add files via upload

--- artificial-intelligence-machine-learning/linear.py
+++ artificial-intelligence-machine-learning/linear.py
@@ -1,22 +1,21 @@
 import matplotlib.pyplot as plt
 from sklearn.datasets import load_iris
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 def linear_regression():
-       iris = load_iris()
-       x = iris.data[:, 0].reshape(-1, 1)
-       y = iris.data[:, 1]
-       x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.16, random_state = 1)
-       model = LinearRegression()
-       model.fit(x_train, y_train)
-       y_pred = model.predict(x_test)
-       plt.scatter(x_test, y_test, color = "blue", label = "Actual")
-       plt.scatter(x_test, y_pred, color = "red", label = "Predicted")
-       plt.plot(x_test, y_pred, color = "green", linewidth = 1, label = "Regression Line")
-       plt.title("Linear Regression on Iris Dataset")
-       plt.xlabel("Feature 1")
-       plt.ylabel("Feature 2")
-       plt.legend()
-       plt.show()
+        iris = load_iris()
+        x = iris.data[:, 0].reshape(-1, 1)
+        y = iris.data[:, 2]
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.16, random_state = 1)
+        model = LinearRegression()
+        model.fit(x_train, y_train)
+        y_pred = model.predict(x_test)
+        plt.scatter(x_test, y_test, color = "blue", label = "Actual")
+        plt.scatter(x_test, y_pred, color = "red", label = "Predicted")
+        plt.plot(x_test, y_pred, color = "green", linewidth = 1, label = "Regression Line")
+        plt.title("Linear Regression on Iris Dataset")
+        plt.xlabel("Feature 1")
+        plt.ylabel("Feature 2")
+        plt.legend()
+        plt.show()
 linear_regression()
